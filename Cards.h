bb0999ec0e80e5b9685b0403482436ff06a0aeb0
Parth-11 <parth12.mgg@gmail.com>
2023-06-12 12:51:37 +0530

    First Commit

---
+++ Cards.h
@@ -0,0 +1,11 @@
+#include <vector>
+#include <string>
+
+class Cards{
+    private:
+        std::vector<std::vector<int>> Matrix;
+        std::vector<std::string> cardType;
+        std::vector<int> cardNum;
+    public:
+        
+};
